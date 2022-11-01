aa462b504bf3623aa6b9a66693a4a26045c04d06
abrark <abrark.asahi@gmail.com>
2022-11-01 16:28:43 +0530

    change for: to use kameri-app as mount-point

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 ./fpm-install.sh
 
-./fpm serve --port $PORT --bind 0.0.0.0 --download-base-url https://raw.githubusercontent.com/fifthtry/kameri-app/main
+./fpm serve --port $PORT --bind 0.0.0.0 --download-base-url https://raw.githubusercontent.com/fifthtry/kameri-app/as-mount-point/
 
 # Heroku create app
 # heroku apps:create kameri-app
 # heroku apps:destroy kameri-app
 # heroku git:remote -a kameri-app
 # git push heroku main
+
+# kameri.herokuapp.com
+
+# deployed another branch
+# Heroku create app
+# heroku apps:create kameri
+# heroku apps:destroy kameri
+# heroku git:remote -a kameri
+# git push heroku as-mount-point
