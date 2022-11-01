9dc9a47927d5c87781729dc46f934ba31f5e6b88
abrark <abrark.asahi@gmail.com>
2022-11-01 15:59:04 +0530

    update fpm serve

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./fpm-install.sh
 
-./fpm serve $PORT --bind 0.0.0.0 --download-base-url https://raw.githubusercontent.com/fifthtry/kameri-app/main
+./fpm serve --port $PORT --bind 0.0.0.0 --download-base-url https://raw.githubusercontent.com/fifthtry/kameri-app/main
 
 # Heroku create app
 # heroku apps:create kameri-app
